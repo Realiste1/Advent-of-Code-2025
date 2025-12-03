4c230a3006cf1efde7393e7c544341f5ad19dbbd
Realiste <nicolasmatasoff@gmail.com>
2025-12-02 23:40:06 -0500

    Add first 2 days

---
+++ Day 1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+string[] Lines = File.ReadAllLines("code.txt");
+
+int dial = 50;
+int counter = 0;
+
+foreach (string line in Lines)
+{
+	int number = Convert.ToInt32(line.Remove(0, 1));
+
+	if (line[0] == 'L')
+		dial -= number;
+	else
+		dial += number;
+
+	dial %= 100;
+
+	if (dial == 0)
+		counter++;
+}
+
+Console.WriteLine($"Part 1: {counter}");
+
+// Part 2
+counter = 0;
+dial = 50;
+
+foreach (string line in Lines)
+{
+	int number = Convert.ToInt32(line.Remove(0, 1));
+
+	for (int i = 0; i < number; i++)
+	{
+		if (line[0] == 'L')
+			dial--;
+		else
+			dial++;
+
+		if (dial == 0)
+			counter++;
+		if (dial < 0)
+			dial += 100;
+		if (dial == 100)
+		{
+			counter++;
+			dial -= 100;
+		}
+	}
+}
+
+Console.WriteLine($"Part 2: {counter}");
---
+++ Day 2/Program.cs
@@ -0,0 +1,61 @@
+﻿string[] patterns = File.ReadAllText("input.txt").Split(',');
+
+long result = 0;
+
+foreach (string pattern in patterns)
+{
+	long start = Convert.ToInt64(pattern.Split('-')[0]);
+	long end = Convert.ToInt64(pattern.Split('-')[1]);
+
+	for (long i = start; i <= end; i++)
+	{
+		int length = i.ToString().Length;
+		if (length % 2 == 1)
+			continue;
+		long half = Convert.ToInt32(i.ToString().Remove(length / 2));
+		if (i == Convert.ToInt64($"{half}{half}"))
+		{
+			result += i;
+		}
+	}
+}
+
+
+Console.WriteLine($"Partie 1 = {result}");
+
+// Part 2
+result = 0;
+
+List<long> invalids = new List<long>();
+
+foreach (string pattern in patterns)
+{
+	long start = Convert.ToInt64(pattern.Split('-')[0]);
+	long end = Convert.ToInt64(pattern.Split('-')[1]);
+
+	for (long i = start; i <= end; i++)
+	{
+		int length = i.ToString().Length;
+		for (int j = 1; j <= length/2; j++)
+		{
+			if (length % j != 0)
+				continue;
+			long loop = Convert.ToInt64(i.ToString().Remove(j));
+			string loopedNumber = "";
+			for (int k = 1; k <= length / j; k++)
+				loopedNumber += loop;
+			if (i.ToString() == loopedNumber)
+			{
+				if(!invalids.Contains(i))
+					invalids.Add(i);
+			}
+		}
+	}
+}
+
+foreach (long invalid in invalids)
+{
+	result += invalid;
+}
+
+Console.WriteLine($"Partie 2 = {result}");
